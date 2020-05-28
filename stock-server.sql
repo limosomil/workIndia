cf6bc0a9f78dcf63ba10f82214320a9e5de78a1b
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2020-05-28 13:56:46 +0530

    Database structure changed.

--- stock-server.sql
+++ stock-server.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 27, 2020 at 09:46 AM
+-- Generation Time: May 28, 2020 at 08:26 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -13,6 +13,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `stock_game`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `couponcode`
+--
+
+CREATE TABLE `couponcode` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `code` text,
+  `date_added` datetime DEFAULT NULL,
+  `expiry_date` datetime DEFAULT NULL,
+  `amount` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -31,101 +46,13 @@ CREATE TABLE `otp_data` (
 -- Dumping data for table `otp_data`
 --
 
--- INSERT INTO `otp_data` (`id`, `otp`, `phone`, `date_created`) VALUES
--- (4, '809058', '9876543210', '2020-05-26 22:55:53');
+INSERT INTO `otp_data` (`id`, `otp`, `phone`, `date_created`) VALUES
+(4, '809058', '9876543210', '2020-05-26 22:55:53');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_data`
---
-
-CREATE TABLE `user_data` (
-  `id` int(11) NOT NULL,
-  `first_name` text,
-  `last_name` text,
-  `phone` text NOT NULL,
-  `date_registered` datetime NOT NULL,
-  `last_login` datetime DEFAULT NULL,
-  `last_login_ip` text,
-  `FCM_token` text,
-  `login_token` text,
-  `usr_setupdone` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user_data`
---
-
--- INSERT INTO `user_data` (`id`, `first_name`, `last_name`, `phone`, `date_registered`, `last_login`, `last_login_ip`, `FCM_token`, `login_token`, `usr_setupdone`) VALUES
--- (10, NULL, NULL, '7666066985', '2020-05-26 02:04:22', NULL, NULL, NULL, NULL, 0),
--- (11, NULL, NULL, '9876543210', '2020-05-26 10:40:54', NULL, NULL, NULL, NULL, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `wallet`
---
-
-CREATE TABLE `wallet` (
-  `id` int(11) DEFAULT NULL,
-  `balance` double NOT NULL DEFAULT '0',
-  `funds_added` double NOT NULL DEFAULT '0',
-  `funds_withdrawm` double NOT NULL DEFAULT '0',
-  `money_won` double NOT NULL DEFAULT '0',
-  `money_played` double NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `otp_data`
---
-ALTER TABLE `otp_data`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `otp` (`otp`);
-
---
--- Indexes for table `user_data`
---
-ALTER TABLE `user_data`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `wallet`
---
-ALTER TABLE `wallet`
-  ADD KEY `id` (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `otp_data`
---
-ALTER TABLE `otp_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `user_data`
---
-ALTER TABLE `user_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `wallet`
---
-ALTER TABLE `wallet`
-  ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user_data` (`id`);
-
-  -- Table structure for table `stocks`
+-- Table structure for table `stocks`
 --
 
 CREATE TABLE `stocks` (
@@ -148,9 +75,57 @@ CREATE TABLE `stocks` (
   `NETCHG` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_data`
+--
+
+CREATE TABLE `user_data` (
+  `id` int(11) NOT NULL,
+  `first_name` text,
+  `last_name` text,
+  `phone` varchar(10) NOT NULL,
+  `date_registered` datetime NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `last_login_ip` text,
+  `FCM_token` text,
+  `login_token` text,
+  `usr_setupdone` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `id` int(11) DEFAULT NULL,
+  `balance` double NOT NULL DEFAULT '0',
+  `funds_added` double NOT NULL DEFAULT '0',
+  `funds_withdrawm` double NOT NULL DEFAULT '0',
+  `money_won` double NOT NULL DEFAULT '0',
+  `money_played` double NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `couponcode`
+--
+ALTER TABLE `couponcode`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `phone` (`phone`);
+
+--
+-- Indexes for table `otp_data`
+--
+ALTER TABLE `otp_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `otp` (`otp`);
 
 --
 -- Indexes for table `stocks`
@@ -159,11 +134,58 @@ ALTER TABLE `stocks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_data`
+--
+ALTER TABLE `user_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone` (`phone`);
+
+--
+-- Indexes for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD KEY `wallet_ibfk_1` (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `couponcode`
+--
+ALTER TABLE `couponcode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `otp_data`
+--
+ALTER TABLE `otp_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `stocks`
 --
 ALTER TABLE `stocks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_data`
+--
+ALTER TABLE `user_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `couponcode`
+--
+ALTER TABLE `couponcode`
+  ADD CONSTRAINT `couponcode_ibfk_1` FOREIGN KEY (`phone`) REFERENCES `user_data` (`phone`);
+
+--
+-- Constraints for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user_data` (`id`);
